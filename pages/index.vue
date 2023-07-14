08e356677404dd28fe07ae7d5cda4ad1c81fce43
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2023-07-14 17:52:07 +0200

    feat: add i18n

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
+<script setup>
+const { locale } = useI18n()
+</script>
+
 <template>
   <div>
-    index
+    <form>
+      <select v-model="locale">
+        <option value="en">en</option>
+        <option value="fr">fr</option>
+      </select>
+      <p>{{ $t('welcome') }}</p>
+    </form>
   </div>
 </template>
